00ebdc70ba7e1f44ad232b16cf3921d2bbcf3660
Venkata Sai Suvvari <95087212+Venkatasai-102@users.noreply.github.com>
2023-03-21 12:24:39 +0530

    Update main.hpp

--- Assign5/main.hpp
+++ Assign5/main.hpp
@@ -8,32 +8,32 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <queue>
+#include <time.h>
 using namespace std;
 
-
 extern int guests_entered = 0, guests_done = 0;
-extern sem_t exculsive_cleaner; //mutex
+extern sem_t exculsive_cleaner; // mutex
 extern sem_t guest_entered_sem, guest_cleaner_sem;
 extern sem_t cleaner_sem, guest_sem, room_sem;
 extern int cleaners = 0;
 
 extern int x, y, n;
 
-class Room{
-    public:
-    
-    int guests_used;
-    int time_used;
-    int priority;
-    Room(int priority=-1, int guests_used=0, int time_used = 0): guests_used(guests_used), time_used(time_used), priority(priority){}
+class Room
+{
+public:
+    int guests_used; // number of guests used
+    int time_used; // total time used
+    int priority; // priority of the guest staying in the room
+    Room(int priority = -1, int guests_used = 0, int time_used = 0) : guests_used(guests_used), time_used(time_used), priority(priority) {}
 };
 
 extern Room *room;
 extern void *guest(void *arg);
 extern void *cleaningStaff(void *arg);
 
-extern queue<Room*>freeRooms;
-extern set<pair<int,Room*> >occupiedRooms;
-extern queue<Room *>doneRooms;
+extern queue<Room *> freeRooms;
+extern set<pair<int, Room *>> occupiedRooms;
+extern queue<Room *> doneRooms;
 
 #endif
