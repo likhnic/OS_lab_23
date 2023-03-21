a69b28bddd537abe9cd0f0cad5a6433d691566b1
Venkata Sai Suvvari <95087212+Venkatasai-102@users.noreply.github.com>
2023-03-21 12:25:12 +0530

    Update main.cpp

--- Assign5/main.cpp
+++ Assign5/main.cpp
@@ -1,29 +1,34 @@
 #include "main.hpp"
 
-
-int main(){
-
-    cout<<"Enter x, y, n: ";
-    cin>>x>>y>>n;
+int main()
+{
+    srand(time(NULL));
+    cout << "Enter x, y, n: ";
+    cin >> x >> y >> n;
     pthread_t guests[y], cleaningStaffs[x];
 
     // init semaphores
     sem_init(&room_sem, 0, 1);
     sem_init(&cleaner_sem, 0, 0);
 
-    for(int i=0;i<y;++i){
-        pthread_create(&guests[i], NULL, guest, (void *)&i);
+    for (int i = 0; i < y; ++i)
+    {
+        int k = rand() % y;
+        pthread_create(&guests[i], NULL, guest, (void *)&k);
     }
 
-    for(int i=0;i<x;++i){
+    for (int i = 0; i < x; ++i)
+    {
         pthread_create(&cleaningStaffs[i], NULL, cleaningStaff, (void *)&i);
     }
 
-    for(int i=0;i<y;++i){
+    for (int i = 0; i < y; ++i)
+    {
         pthread_join(guests[i], NULL);
     }
 
-    for(int i=0;i<x;++i){
+    for (int i = 0; i < x; ++i)
+    {
         pthread_join(cleaningStaffs[i], NULL);
     }
 
